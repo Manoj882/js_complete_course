2e9ab28d97073490318dfdd53df4183d1d4cc17a
Manoj882 <manojbk488@gmail.com>
2023-10-24 12:06:48 +0545

    reset the game

--- 04.DOM/03.Pig-Game/script.js
+++ 04.DOM/03.Pig-Game/script.js
@@ -1,8 +1,8 @@
 'use strict';
 
 //Selecting Elements
-const player0 = document.querySelector('.player--0');
-const player1 = document.querySelector('.player--1');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1'); // no need to include #. # and dot (.) are only used in querySelector
 const current0El = document.getElementById('current--0');
@@ -14,15 +14,26 @@ const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 // Starting Conditions
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
-const scores = [0, 0];
+let scores, currentScore, activePlayer, playing;
 
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function () {
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+
+init();
 
 const switchPlayer = function () {
   document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -81,3 +92,5 @@ btnHold.addEventListener('click', function () {
     }
   }
 });
+
+btnNew.addEventListener('click', init);
