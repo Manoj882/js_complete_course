53a0212ac2b55bb83d7b9e2c978acc18719e02b3
Manoj882 <manojbk488@gmail.com>
2023-10-28 09:00:00 +0545

    pass default parameter in function

---
+++ 07.A-Closer-Look-At-Function/01.Default-Parameter/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const bookings = [];
+
+const createBooking = function (
+  flightNum,
+  numPassengers = 1,
+  price = 199 * numPassengers
+) {
+  // ES5
+  // numPassengers = numPassengers || 1;
+  // price ||= 199;
+
+  const booking = {
+    flightNum,
+    numPassengers,
+    price,
+  };
+
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH234', 2, 250);
+createBooking('LH567', 3);
+createBooking('LH567', 5);
+
+createBooking('LH123', undefined, 1000); // set undefined for numPassenger
